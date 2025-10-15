6343fdeaaa28a4f96733eceaee99bab2cdbdf5fe
ts5516 <science1528@gmail.com>
2025-10-15 14:59:41 +0900

    [COM-4] / mqtt & http 같이 자동실행되도록 변경

--- ota/run_mqtt_broker.bat
+++ ota/run_mqtt_broker.bat
@@ -1,8 +1,31 @@
 @echo off
 chcp 65001 >nul
-echo [1/2] Starting MQTT Broker...
-call "C:\Program Files\mosquitto\mosquitto.exe" -c "broker\mosquitto.conf" -v
+title OTA MQTT Broker + HTTP Server
 
-echo [2/2] Ready to send OTA JSON file.
-cd /d "%~dp0publisher"
-cmd /k
+cd /d %~dp0
+
+echo ==========================================
+echo        OTA MQTT Broker + HTTP Server
+echo ==========================================
+echo.
+
+:: 1) MQTT 브로커 실행
+echo [1/2] MQTT 브로커를 실행합니다...
+start "Mosquitto Broker" cmd /k "mosquitto.exe -c broker\mosquitto.conf -v"
+timeout /t 3 >nul
+
+:: 2) HTTP 서버 실행 (ota/publisher/build 폴더 기준)
+echo [2/2] HTTP 서버를 실행합니다 (포트 8000)...
+cd publisher\build
+start "" cmd /k "python -m http.server 8000"
+cd ../..
+
+echo.
+echo ==========================================
+echo   모든 서버가 정상적으로 실행되었습니다.
+echo   MQTT Broker 포트 : 1883
+echo   HTTP Server 포트 : 8000
+echo   (파일 경로: ota/publisher/build)
+echo ==========================================
+echo.
+pause
